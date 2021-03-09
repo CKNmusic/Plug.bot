5e7f74199c0e1f74f6836f4fd82240e76e69e7fc
OtavioMariano <musicckn@gmail.com>
2021-03-08 23:17:37 -0300

    1
    first

---
+++ Assets/Scripts/Lights/rotateLights.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateLights : MonoBehaviour
+{
+    [Range(-10, 10)] public int velocidade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,5 * velocidade * Time.deltaTime, 0);
+    }
+}
